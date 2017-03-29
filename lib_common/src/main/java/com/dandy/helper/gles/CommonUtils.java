14b93a662cac8723f667e4ad8ece2137b0949ef9
flycatdeng <dengchukun@qq.com>
2017-03-29 10:13:20 +0800

    gles：添加环境是否支持某种扩展的判断

--- lib_common/src/main/java/com/dandy/helper/gles/CommonUtils.java
+++ lib_common/src/main/java/com/dandy/helper/gles/CommonUtils.java
@@ -7,8 +7,9 @@ import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLES20;
 
+import javax.microedition.khronos.opengles.GL10;
+
 /**
- * 
  * @author dengchukun 2016年11月23日
  */
 public class CommonUtils {
@@ -20,7 +21,7 @@ public class CommonUtils {
      * 检查设备是否支持OpenGL ES 2.0
      * check whether the device support OpenGL ES 2.0
      * </pre>
-     * 
+     *
      * @param context
      * @return
      */
@@ -34,9 +35,8 @@ public class CommonUtils {
 
     /**
      * 检查每一步操作是否有错误的方法
-     * 
-     * @param op
-     *            TAG
+     *
+     * @param op TAG
      */
     public static void checkGlError(String op) {
         int error;
@@ -44,5 +44,38 @@ public class CommonUtils {
             LogHelper.d("ES20_ERROR", op + ": glError " + error);
             throw new RuntimeException(op + ": glError " + error);
         }
+    }
+
+    /**
+     * <pre>
+     *     判断环境是否支持FBO
+     * </pre>
+     * @param gl
+     * @return
+     */
+    public static boolean checkIfContextSupportsFrameBufferObject(GL10 gl) {
+        return checkIfContextSupportsExtension(gl, "GL_OES_framebuffer_object");
+    }
+
+    /**
+     * <pre>
+     * 判断环境是否支持某种扩展
+     * This is not the fastest way to check for an extension, but fine if
+     * we are only checking for a few extensions each time a context is created.
+     * </pre>
+     *
+     * @param gl
+     * @param extension
+     * @return true if the extension is present in the current context.
+     */
+    public static boolean checkIfContextSupportsExtension(GL10 gl, String extension) {
+        String extensions = " " + gl.glGetString(GL10.GL_EXTENSIONS) + " ";
+        // The extensions string is padded with spaces between extensions, but not
+        // necessarily at the beginning or end. For simplicity, add spaces at the
+        // beginning and end of the extensions string and the extension string.
+        // This means we can avoid special-case checks for the first or last
+        // extension, as well as avoid special-case checks when an extension name
+        // is the same as the first part of another extension name.
+        return extensions.indexOf(" " + extension + " ") >= 0;
     }
 }
--- lib_common/src/main/java/com/dandy/helper/gles/TextureHelper.java
+++ lib_common/src/main/java/com/dandy/helper/gles/TextureHelper.java
@@ -7,11 +7,14 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.opengl.GLES11Ext;
 import android.opengl.GLES20;
 import android.opengl.GLUtils;
 
 import com.dandy.helper.android.LogHelper;
 import com.dandy.helper.java.ObjectHelper;
+
+import javax.microedition.khronos.opengles.GL10;
 
 @SuppressLint("NewApi")
 public class TextureHelper {
@@ -135,6 +138,23 @@ public class TextureHelper {
             bitmapTmp.recycle(); // 纹理加载成功后释放图片
         }
         return textureId;
+    }
+
+    public static int getExternalOESTextureID() {
+        int[] texture = new int[1];
+
+        GLES20.glGenTextures(1, texture, 0);
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, texture[0]);
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameterf(GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_WRAP_S, GL10.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
+                GL10.GL_TEXTURE_WRAP_T, GL10.GL_CLAMP_TO_EDGE);
+
+        return texture[0];
     }
 
     /**
