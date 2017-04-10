262eba6ae4192a9948632bb98eab1e2930b9b82b
flycatdeng <dengchukun@qq.com>
2017-04-10 15:36:12 +0800

    gles_engine:Actor构造器带有Context

--- lib_gles_engine/src/main/java/com/dandy/gles/engine/Actor.java
+++ lib_gles_engine/src/main/java/com/dandy/gles/engine/Actor.java
@@ -1,5 +1,11 @@
 package com.dandy.gles.engine;
 
-public class Actor extends Base{
+import android.content.Context;
 
+public class Actor extends Base {
+    protected Context mContext;
+
+    public Actor(Context context) {
+        mContext = context;
+    }
 }
