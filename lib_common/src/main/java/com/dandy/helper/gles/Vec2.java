78c2979a9426f4ff2ea4a80eca2824a4a9ef9c20
flycatdeng <dengchukun@qq.com>
2017-04-14 18:06:40 +0800

    gles_common:优化2 3维向量单位化的方法调用

---
+++ lib_common/src/main/java/com/dandy/helper/gles/Vec2.java
@@ -0,0 +1,64 @@
+package com.dandy.helper.gles;
+
+import com.dandy.helper.java.basedata.FloatHelper;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ * Created by flycatdeng on 2017/4/14.
+ * Email:dengchukun@qq.com
+ * Wechat:flycatdeng
+ */
+
+public class Vec2 {
+    public float x;
+    public float y;
+
+    public Vec2() {
+
+    }
+
+    public Vec2(float sameValue) {
+        this(sameValue, sameValue);
+    }
+
+    public Vec2(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * 向量规格化
+     *
+     * @return
+     */
+    public Vec2 normalize() {
+        return floatToVec(FloatHelper.normalize(this.vecToFloat()));
+    }
+
+    public Vec2 add(Vec2 vec2) {
+        Vec2 result = new Vec2();
+        result.x = x + vec2.x;
+        result.y = y + vec2.y;
+        return result;
+    }
+
+    public float[] vecToFloat() {
+        return new float[]{x, y};
+    }
+
+    public static float[] vecToFloat(Vec2 vec) {
+        return new float[]{vec.x, vec.y};
+    }
+
+    public static Vec2 floatToVec(float[] floats) {
+        if (floats.length == 1) {
+            return new Vec2(floats[0]);
+        } else if (floats.length == 2) {
+            return new Vec2(floats[0], floats[1]);
+        } else {
+            throw new RuntimeException("floats array list length must be 1 or 2");
+        }
+    }
+}
---
+++ lib_common/src/main/java/com/dandy/helper/gles/Vec3.java
@@ -0,0 +1,67 @@
+package com.dandy.helper.gles;
+
+import com.dandy.helper.java.basedata.FloatHelper;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ * Created by flycatdeng on 2017/4/12.
+ * Email:dengchukun@qq.com
+ * Wechat:flycatdeng
+ */
+
+public class Vec3 {
+    public float x;
+    public float y;
+    public float z;
+
+    public Vec3() {
+
+    }
+
+    public Vec3(float sameValue) {
+        this(sameValue, sameValue, sameValue);
+    }
+
+    public Vec3(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * 向量规格化
+     *
+     * @return
+     */
+    public Vec3 normalize() {
+        return floatToVec(FloatHelper.normalize(this.vecToFloat()));
+    }
+
+    public Vec3 add(Vec3 vec3) {
+        Vec3 result = new Vec3();
+        result.x = x + vec3.x;
+        result.y = y + vec3.y;
+        result.z = z + vec3.z;
+        return result;
+    }
+
+    public float[] vecToFloat() {
+        return new float[]{x, y, z};
+    }
+
+    public static float[] vecToFloat(Vec3 vec3) {
+        return new float[]{vec3.x, vec3.y, vec3.z};
+    }
+
+    public static Vec3 floatToVec(float[] floats) {
+        if (floats.length == 1) {
+            return new Vec3(floats[0]);
+        } else if (floats.length == 3) {
+            return new Vec3(floats[0], floats[1], floats[2]);
+        } else {
+            throw new RuntimeException("floats array list length must be 1 or 3");
+        }
+    }
+}
