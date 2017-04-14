5acba1321b7c313cadd742f7fe2ea176dcceab96
flycatdeng <dengchukun@qq.com>
2017-04-14 14:59:05 +0800

    gles_common:Vector3D添加带参构造器

--- lib_common/src/main/java/com/dandy/helper/gles/Vector3D.java
+++ lib_common/src/main/java/com/dandy/helper/gles/Vector3D.java
@@ -14,6 +14,16 @@ public class Vector3D {
     public float y;
     public float z;
 
+    public Vector3D() {
+
+    }
+
+    public Vector3D(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     /**
      * 向量规格化
      *
